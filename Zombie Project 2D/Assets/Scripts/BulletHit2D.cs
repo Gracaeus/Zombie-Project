9e0e23324feaf157730ed0a3ddb6db11a5863f01
Gracaeus <tictacasian@gmail.com>
2018-03-05 13:21:53 +0000

    Part2

---
+++ Zombie Project 2D/Assets/Scripts/BulletHit2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHit2D : MonoBehaviour { 
+    public int damage = 1;
+    public string damageTag = "";
+
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other) {
+        if (other.CompareTag(damageTag))
+        {
+            other.SendMessage("TakeDamage", damage);
+        }
+        Destroy(gameObject);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
