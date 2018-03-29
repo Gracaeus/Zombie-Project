d2a99c985c5233cc2661cf0d2904f48f4d0832ff
Kenji Honculada <kenji@space-cowboy.home>
2018-03-29 09:45:32 +0100

    Revert "Crawler animation"
    This reverts commit 2c9d808ecb8a3f73bb69e513c1efc76f94ef9ff3.

--- Zombie Project 2D/Assets/Scripts/PlayerBehaviour.cs
+++ Zombie Project 2D/Assets/Scripts/PlayerBehaviour.cs
@@ -1,19 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-	public int playerHealth = 10;
+	public int playerHealth = 100;
+	public int playerCurrentHealth= playerHealth;
+	public Slider healthBar;
+	public Image fillImage;
+	public Color fullHealth = Color.red;
+	public Color zeroHealth = Color.black;
 
 	// Use this for initialization
 	public void PlayerTakeDamage(int damage)
 	{
-		playerHealth -= damage;
+		playerCurrentHealth -= damage;
+		SetHealthUI ();
 		print ("player hurt " + playerHealth);
 		if (playerHealth <= 0)
 		{
 			Destroy(gameObject);
 		}
 	}
+	public void SetHealthUI()
+	{
+		healthBar.value = playerCurrentHealth;
+		fillImage.color = Color.Lerp (zeroHealth, fullHealth, playerCurrentHealth / playerHealth);
+	}
+		
 }
